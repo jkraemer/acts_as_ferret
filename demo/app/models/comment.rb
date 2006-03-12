99b421d5f6607aadf1ccbe733bce647473507211
jk <jk@7326d000-0a0e-0410-9cad-a9b28e7838dc>
2006-03-12 11:22:45 +0000

    sqlite schema, added tests for rebuild_index and custom to_doc method
    git-svn-id: svn://projects.jkraemer.net/acts_as_ferret/trunk@14 7326d000-0a0e-0410-9cad-a9b28e7838dc

--- demo/app/models/comment.rb
+++ demo/app/models/comment.rb
@@ -1,6 +1,22 @@
+require 'ferret'
+
 class Comment < ActiveRecord::Base
   belongs_to :parent, :class_name => 'Content'
   # just index all fields:
   acts_as_ferret 
   #acts_as_ferret :fields => ['author', 'content' ]
+
+  # you can override the default to_doc method 
+  # to customize what gets into your index. 
+  def to_doc
+    # doc now has all the fields of our model instance, we 
+    # just add another field to it:
+    doc = super
+    # add a field containing the current time
+    doc <<  Ferret::Document::Field.new(
+              'added', Time.now.to_i.to_s, 
+              Ferret::Document::Field::Store::YES, 
+              Ferret::Document::Field::Index::UNTOKENIZED)
+    return doc
+  end
 end
--- demo/test/unit/comment_test.rb
+++ demo/test/unit/comment_test.rb
@@ -12,6 +12,26 @@ class CommentTest < Test::Unit::TestCase
     assert_equal "#{RAILS_ROOT}/index/test/Comment", Comment.class_index_dir
   end
 
+  # tests the automatic building of an index when none exists
+  # delete index/test/* before running rake to make this useful
+  def test_index_rebuild
+    comments_from_ferret = Comment.find_by_contents('"comment from fixture"')
+    assert_equal 2, comments_from_ferret.size
+    assert comments_from_ferret.include?(comments(:first))
+    assert comments_from_ferret.include?(comments(:another))
+  end
+
+  # tests the custom to_doc method defined in comment.rb
+  def test_custom_to_doc
+    top_docs = Comment.ferret_index.search('"comment from fixture"')
+    assert_equal 2, top_docs.score_docs.size
+    doc = Comment.ferret_index.doc(top_docs.score_docs[0].doc)
+    # check for the special field added by the custom to_doc method
+    assert_not_nil doc[:added]
+    # still a valid int ?
+    assert doc[:added].to_i > 0
+  end
+
   def test_find_by_contents
     comment = Comment.new( :author => 'john doe', :content => 'This is a useless comment' )
     comment.save
@@ -44,8 +64,10 @@ class CommentTest < Test::Unit::TestCase
     assert comments_from_ferret.empty?
     # ...unless you connect them by OR
     comments_from_ferret = Comment.find_by_contents('monkey OR comment')
-    assert_equal 1, comments_from_ferret.size
-    assert_equal comment.id, comments_from_ferret.first.id
+    assert_equal 3, comments_from_ferret.size
+    assert comments_from_ferret.include?(comment)
+    assert comments_from_ferret.include?(comments(:first))
+    assert comments_from_ferret.include?(comments(:another))
 
     # multiple terms, each term has to occur in a document to be found, 
     # but they may occur in different fields
