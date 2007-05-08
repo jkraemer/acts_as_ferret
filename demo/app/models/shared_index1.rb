cd7aa4921de8957676a1ea531a9d8380d788aaa9
jk <jk@7326d000-0a0e-0410-9cad-a9b28e7838dc>
2007-05-08 09:48:33 +0000

    new way to pass ferret options to acts_as_ferret
    git-svn-id: svn://projects.jkraemer.net/acts_as_ferret/trunk@186 7326d000-0a0e-0410-9cad-a9b28e7838dc

--- demo/app/models/shared_index1.rb
+++ demo/app/models/shared_index1.rb
@@ -1,8 +1,9 @@
 class SharedIndex1 < ActiveRecord::Base
   # default field list for all classes sharing the index
   DEFAULT_FIELDS = [ :name ]
-  acts_as_ferret( { :fields       => { :name => { :store => :yes } }, 
-                    :single_index => true, 
-                    :remote       => ENV['AAF_REMOTE'] },
-                  { :default_field => DEFAULT_FIELDS } )
+  acts_as_ferret( :fields       => { :name => { :store => :yes } }, 
+                  :single_index => true, 
+                  :remote       => ENV['AAF_REMOTE'],
+                  :ferret       => { :default_field => DEFAULT_FIELDS } 
+                )
 end
