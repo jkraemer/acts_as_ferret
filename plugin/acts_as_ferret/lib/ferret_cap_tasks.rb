5aa5b88aa09ee7701be979fd24eb1d489501e0a9
jk <jk@7326d000-0a0e-0410-9cad-a9b28e7838dc>
2007-05-13 20:49:29 +0000

    capistrano task documentation
    git-svn-id: svn://projects.jkraemer.net/acts_as_ferret/trunk@188 7326d000-0a0e-0410-9cad-a9b28e7838dc

--- plugin/acts_as_ferret/lib/ferret_cap_tasks.rb
+++ plugin/acts_as_ferret/lib/ferret_cap_tasks.rb
@@ -1,3 +1,9 @@
+# Ferret DRb server Capistrano tasks
+# Usage:
+# Add require 'vendor/plugins/acts_as_ferret/lib/ferret_cap_tasks' to your
+# config/deploy.rb
+# call ferret.restart where you restart your Mongrels.
+# ferret.stop and ferret.start are available, too.
 module FerretCapTasks
   def start
     run "cd #{current_path}; RAILS_ENV=production script/ferret_start"
