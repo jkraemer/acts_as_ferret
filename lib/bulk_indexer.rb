3284e8e8503639414b2e85e7b35bffd3bc142bd7
Jens Kraemer <jk@jkraemer.net>
2010-01-30 15:24:36 +0100

    fix timing output for index rebuild / bulk indexing

--- lib/bulk_indexer.rb
+++ lib/bulk_indexer.rb
@@ -5,6 +5,8 @@ module ActsAsFerret
       @logger = args[:logger]
       @model = args[:model]
       @work_done = 0
+      @indexed_records = 0
+      @total_time = 0.0
       @index = args[:index]
       if args[:reindex]
         @reindex = true
@@ -19,18 +21,34 @@ module ActsAsFerret
         docs = []
         records.each { |rec| docs << [rec.to_doc, rec.ferret_analyzer] if rec.ferret_enabled?(true) }
         @index.update_batch(docs)
-        # records.each { |rec| @index.add_document(rec.to_doc, rec.ferret_analyzer) if rec.ferret_enabled?(true) }
       }.to_f
-      @work_done = offset.to_f / @model_count * 100.0 if @model_count > 0
-      remaining_time = ( batch_time / @batch_size ) * ( @model_count - offset + @batch_size )
+      rec_count = records.size
+      @indexed_records += rec_count
+      @total_time += batch_time
+      @work_done = @indexed_records.to_f / @model_count * 100.0 if @model_count > 0
+      @logger.debug "took #{batch_time} to index last #{rec_count} records. #{records_waiting} records to go. Avg time per record: #{avg_time_per_record}"
+      remaining_time = avg_time_per_record * records_waiting
       @logger.info "#{@reindex ? 're' : 'bulk '}index model #{@model.name} : #{'%.2f' % @work_done}% complete : #{'%.2f' % remaining_time} secs to finish"
-
     end
-
+    
     def measure_time
       t1 = Time.now
       yield
       Time.now - t1
+    end
+    
+    protected
+    
+    def avg_time_per_record
+      if @indexed_records > 0
+        @total_time / @indexed_records
+      else
+        0
+      end
+    end
+    
+    def records_waiting
+      @model_count - @indexed_records
     end
 
   end
