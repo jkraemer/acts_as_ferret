219c0a39935e27c7717e7e55d79720a351be7192
Jens Kraemer <jk@jkraemer.net>
2010-08-03 18:50:20 +0200

    declare rake tasks in railtie

--- lib/acts_as_ferret/railtie.rb
+++ lib/acts_as_ferret/railtie.rb
@@ -5,6 +5,10 @@ module ActsAsFerret
   
   class Railtie < Rails::Railtie
         
+    rake_tasks do
+      load File.join(File.dirname(__FILE__), '../../tasks/ferret.rake')
+    end
+      
     config.to_prepare { ActsAsFerret::load_config }
     
   end
